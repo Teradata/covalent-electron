c82b67e189a6426153a893e74ba3ad13cf4f9fec
Jeremy Smartt <mrjayhawk@users.noreply.github.com>
2016-11-29 11:07:26 -0800

    Use only webpack build and restructure files (#2)
    * initial commit of covalent electron framework
    
    * initial commit of covalent electron framework
    
    * updating Readme to have more detailed description of where app is located after building on Mac.
    
    * adding dist-app to .gitignore
    
    * renamed app from docs to just app
    
    * updating to use covalent 0.9.1
    
    * removing building for mac by default, instead not passing in any build flag so it will automatically detect which OS to use
    
    * Updated to only use webpack for bundling and transcompiling and removed some of the not used build gul files
    
    * moving files to electron dir
    
    * recommiting deleting of scripts and moving of electron files to electron dir after wierd merge issue
    
    * fixing more merge issues

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var gulp = require('gulp-help')(require('gulp'));
+var config = require('../build.conf');
 
-gulp.task('build', 'Compile the code', [
-  'compile-ts',
-  'compile-sass',
-  'move-required-files',
-  'move-electron-required-files'
-]);
+gulp.task('build', 'Copy the electron files', function() {
+  return gulp
+    .src(config.paths.electronrequiredfiles)
+    .pipe(gulp.dest('dist/'));
+});
