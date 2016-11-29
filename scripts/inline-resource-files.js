261da580505556560c800dd1467c1776c7663544
Smartt <js186150@musjs186150-633.td.teradata.com>
2016-11-29 10:55:34 -0800

    recommiting deleting of scripts and moving of electron files to electron dir after wierd merge issue

--- scripts/inline-resource-files.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp-help')(require('gulp'));
-var inlineResources = require('./inline-resources.js');
-var config = require('../build.conf');
-
-gulp.task('inline-resource-files', 'Inline resouces files into JS files', function() {
-  inlineResources([config.paths.deployed]);
-});
