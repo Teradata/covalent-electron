2e2ecfe2979527c03b3f096905d76d1fd2c1635a
Kyle Ledbetter <kyle@kyleledbetter.com>
2016-12-01 11:04:38 -0600

    add(covalent-theme): and additional modules (#3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { CovalentHighlightModule } from '@covalent/highlight';
 import { CovalentHttpModule } from '@covalent/http';
 import { CovalentMarkdownModule } from '@covalent/markdown';
 import { CovalentJsonFormatterModule } from '@covalent/json-formatter';
+import { CovalentChartsModule } from '@covalent/charts';
+import { CovalentDataTableModule } from '@covalent/data-table';
+import { CovalentPagingModule } from '@covalent/paging';
+import { CovalentSearchModule } from '@covalent/search';
 
 @NgModule({
   declarations: [
@@ -25,6 +29,11 @@ import { CovalentJsonFormatterModule } from '@covalent/json-formatter';
     CovalentHighlightModule.forRoot(),
     CovalentMarkdownModule.forRoot(),
     CovalentJsonFormatterModule.forRoot(),
+    CovalentChartsModule.forRoot(),
+    CovalentDataTableModule.forRoot(),
+    CovalentMarkdownModule.forRoot(),
+    CovalentPagingModule.forRoot(),
+    CovalentSearchModule.forRoot(),
     appRoutes,
   ], // modules needed to run this module
   providers: [
