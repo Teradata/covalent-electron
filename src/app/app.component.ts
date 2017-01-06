6537c558482cd664e84089ba0fab81d96f392696
Jeremy Smartt <jeremysmartt@users.noreply.github.com>
2017-01-06 08:56:52 -0800

    Upgrade to Covalent 0.10.2, Angular 2.4.1, and Angular/Material 2.0.0-beta.1 (#17)
    * Upgrading to covalent 0.10.2, angular 2.4.1, angular/material 2.0.0-beta.1, angular/router 3.4.1, angular-cli 1.0.0-beta.24
    
    * replaced img with material icon registry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-covalent',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+  constructor(private _iconRegistry: MdIconRegistry,
+              private _domSanitizer: DomSanitizer,
+              viewContainerRef: ViewContainerRef) {
+      this._iconRegistry.addSvgIconInNamespace('assets', 'covalent',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('app/assets/icons/covalent.svg'));
+    }
 
   routes: Object[] = [{
       icon: 'home',
