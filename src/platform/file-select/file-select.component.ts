fd6793f1f8ec9c2e4f5ffa57847be2c59261683f
jeremysmartt <jeremy.smartt@teradata.com>
2017-01-18 16:14:29 -0800

    adding file display to be connected to actual file opened and now have a Back and Forward button to go back and forth between a history of directories the user has selected

--- src/platform/file-select/file-select.component.ts
+++ src/platform/file-select/file-select.component.ts
@@ -5,6 +5,7 @@ export interface IFile {
   name: string;
   icon: string;
   expansion_visible: boolean;
+  contents?: string;
 }
 
 @Component({
@@ -16,20 +17,24 @@ export class TdFileSelectComponent implements AfterViewInit {
 
     files: IFile[] = [];
     prevPath: string = '';
+    currentOpenedFile: IFile;
+    pathHistory: string[] = [];
+    pathFuture: string[] = [];
+    homeDir: string = '';
 
    /**
     * onOpenFileContents: function($event)
     * Event emitted when a file is opened
     */
-    @Output('openFile') openFile: EventEmitter<string> = new EventEmitter<string>();
+    @Output('openFile') openFile: EventEmitter<IFile> = new EventEmitter<IFile>();
 
     ngAfterViewInit(): void {
         // get the node.js "process" object
         let process: any  = electron.remote.process;
         // get the home dir from the process object environment variables
-        let homeDir: string = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-        this.prevPath = homeDir;
-        this.loadFiles('');
+        this.homeDir = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+        this.prevPath = this.homeDir;
+        this.loadFiles('', true);
     }
 
   /**
@@ -37,7 +42,7 @@ export class TdFileSelectComponent implements AfterViewInit {
    * and try to traverse down it to find more files and directories
    * If it is a file then it will get the File Contents
    */
-  loadFiles(pathEnd: string): void {
+  loadFiles(pathEnd: string, clearFuture: boolean): void {
     // append the end of the path to the previous directories that have been traversed
     let newPath: string = path.join(this.prevPath, pathEnd);
     // If it is a directory get traverse more
@@ -54,11 +59,62 @@ export class TdFileSelectComponent implements AfterViewInit {
         this.files.push(fileObj);
       }
       this.prevPath = newPath;
+      this.pathHistory.push(this.prevPath);
+      if (clearFuture) {
+        this.pathFuture = [];
+      }
     // If it is a file then create a dynamic expansion panel and 
     // load in the size and modified date and file contents
     } else {
       let fileContents: string = fs.readFileSync(newPath, 'utf8');
-      this.openFile.emit(fileContents);
+      let file: IFile = {
+        path: newPath.slice(0, newPath.lastIndexOf('/')),
+        name: newPath.slice(newPath.lastIndexOf('/')),
+        icon: 'description',
+        expansion_visible: false,
+        contents: fileContents,
+      };
+      this.currentOpenedFile = file;
+      this.openFile.emit(file);
     }
+  }
+
+  /**
+   * nextPage method to move forward in history of file traversion
+   */
+  nextPage(): void {
+    // remove the path from future
+    let futurePath: string = this.pathFuture.pop();
+    // go to the future path
+    this.prevPath = futurePath;
+    this.loadFiles('', false);
+  }
+
+  /**
+   * previousPage method to move backward in history of file traversion
+   */
+  previousPage(): void {
+    // remove the current path from history
+    let currentPath: string = this.pathHistory.pop();
+    // add the current path to the future
+    this.pathFuture.push(currentPath);
+    // remove the previous path from history and then go to it
+    let previousPath: string = this.pathHistory.pop();
+    this.prevPath = previousPath;
+    this.loadFiles('', false);
+  }
+
+  /**
+   * canShowPrevious method to determine if there are previous results that could be shown
+   */
+  canShowPrevious(): boolean {
+    return (this.pathHistory.length > 1);
+  }
+
+  /**
+   * canShowNext method to determine if there are more results that could be shown
+   */
+  canShowNext(): boolean {
+    return (this.pathFuture.length > 0);
   }
 }
