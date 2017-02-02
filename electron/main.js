30352d6c0508f6a4500f53779f191115db3f4e0c
jeremysmartt <jeremy.smartt@teradata.com>
2017-02-02 09:51:18 -0800

    cleaning up menu code whitespacing

--- electron/main.js
+++ electron/main.js
@@ -63,80 +63,40 @@ function createWindow () {
     {
       label: 'Edit',
       submenu: [
-        {
-          role: 'undo'
-        },
-        {
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'cut'
-        },
-        {
-          role: 'copy'
-        },
-        {
-          role: 'paste'
-        },
-        {
-          role: 'pasteandmatchstyle'
-        },
-        {
-          role: 'delete'
-        },
-        {
-          role: 'selectall'
-        }
+        {role: 'undo'},
+        {role: 'redo'},
+        {type: 'separator'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'pasteandmatchstyle'},
+        {role: 'delete'},
+        {role: 'selectall'}
       ]
     },
     {
       label: 'View',
       submenu: [
-        {
-          role: 'reload'
-        },
-        {
-          role: 'toggledevtools'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'resetzoom'
-        },
-        {
-          role: 'zoomin'
-        },
-        {
-          role: 'zoomout'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'togglefullscreen'
-        }
+        {role: 'reload'},
+        {role: 'toggledevtools'},
+        {type: 'separator'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'},
+        {type: 'separator'},
+        {role: 'togglefullscreen'}
       ]
     },
     {
       role: 'window',
       submenu: [
-        {
-          role: 'minimize'
-        },
-        {
-          role: 'close'
-        }
+        {role: 'minimize'},
+        {role: 'close'}
       ]
     },
-    {
-      role: 'help',
+    {role: 'help',
       submenu: [
-        {
-          label: 'Learn More',
+        {label: 'Learn More',
           click() { require('electron').shell.openExternal('http://electron.atom.io') }
         }
       ]
@@ -147,74 +107,43 @@ function createWindow () {
     template.unshift({
       label: app.getName(),
       submenu: [
-        {
-          role: 'about'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'services',
-          submenu: []
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'hide'
-        },
-        {
-          role: 'hideothers'
-        },
-        {
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'quit'
-        }
+        {role: 'about'},
+        {type: 'separator'},
+        {role: 'services',
+          submenu: []},
+        {type: 'separator'},
+        {role: 'hide'},
+        {role: 'hideothers'},
+        {role: 'unhide'},
+        {type: 'separator'},
+        {role: 'quit'}
       ]
     })
     // Edit menu.
     template[1].submenu.push(
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Speech',
+      {type: 'separator'},
+      {label: 'Speech',
         submenu: [
-          {
-            role: 'startspeaking'
-          },
-          {
-            role: 'stopspeaking'
-          }
+          {role: 'startspeaking'},
+          {role: 'stopspeaking'}
         ]
       }
     )
     // Window menu.
     template[3].submenu = [
-      {
-        label: 'Close',
+      {label: 'Close',
         accelerator: 'CmdOrCtrl+W',
         role: 'close'
       },
-      {
-        label: 'Minimize',
+      {label: 'Minimize',
         accelerator: 'CmdOrCtrl+M',
         role: 'minimize'
       },
-      {
-        label: 'Zoom',
+      {label: 'Zoom',
         role: 'zoom'
       },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Bring All to Front',
+      {type: 'separator'},
+      {label: 'Bring All to Front',
         role: 'front'
       }
     ]
