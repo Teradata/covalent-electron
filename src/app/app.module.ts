460c7a73c61f2781baff582467c465419bf5b743
Jeremy Smartt <jeremysmartt@users.noreply.github.com>
2017-05-01 15:00:47 -0700

    Upgrade to Covalent 1.0.0-beta.3-2, Angular 4.0.0, Electron 1.6.6 (#35)
    * Upgrading to Covalent 1.0.0-beta.3-2, Angular 4.0.0, and Electron 1.6.6 and required dependencies
    
    * Upgrading to Covalent 1.0.0-beta.3-2, Angular 4.0.0, and Electron 1.6.6 and required dependencies
    
    * Upgrading to Covalent 1.0.0-beta.3-2, Angular 4.0.0, and Electron 1.6.6 and required dependencies
    
    * Upgrading to Covalent 1.0.0-beta.3-2, Angular 4.0.0, and Electron 1.6.6 and required dependencies
    
    * Upgrading to Covalent 1.0.0-beta.3-2, Angular 4.0.0, and Electron 1.6.6 and required dependencies
    
    * Need to import BrowserAnimationsModule now, no longer part of core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -9,7 +10,6 @@ import { CovalentCoreModule } from '@covalent/core';
 import { CovalentHttpModule } from '@covalent/http';
 import { CovalentHighlightModule } from '@covalent/highlight';
 import { CovalentMarkdownModule } from '@covalent/markdown';
-import { CovalentChartsModule } from '@covalent/charts';
 import { CovalentMonacoEditorModule } from '../platform/monaco-editor';
 import { CovalentFileSelectModule } from '../platform/file-select';
 
@@ -24,10 +24,10 @@ import { CovalentFileSelectModule } from '../platform/file-select';
     CovalentHttpModule.forRoot(),
     CovalentHighlightModule.forRoot(),
     CovalentMarkdownModule.forRoot(),
-    CovalentChartsModule.forRoot(),
     CovalentMarkdownModule.forRoot(),
     CovalentMonacoEditorModule.forRoot(),
     CovalentFileSelectModule.forRoot(),
+    BrowserAnimationsModule,
     appRoutes,
   ], // modules needed to run this module
   providers: [
